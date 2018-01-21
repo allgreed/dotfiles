5fdbed51b16814c8e79f2e2cd821599b3ca5213a
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2018-01-21 13:12:04 +0100

    Renamed scripts and add to source controll

---
+++ scripts/lock.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# saves the state of mute
+#~/aliases/volume mstat > /tmp/muted 
+
+# in mem path
+screen_path="/mnt/ramdisk/screen.png"
+
+# does a screenshot
+scrot -z $screen_path
+
+# fuzz
+convert $screen_path -filter point -resize 10% -resize 1000% -paint 3 $screen_path
+
+# main
+#~/aliases/volume mute
+i3lock -n -i $screen_path
+
+# returns to previes mute state
+#if [ `cat /tmp/muted` = "false" ]
+#then
+#    ~/aliases/volume unmute
+#fi
+
