a26cc04294aa2f720abf7209a394241fd200df81
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2018-10-25 19:22:35 +0200

    Control session prefixing

--- scripts/run-tmux.sh
+++ scripts/run-tmux.sh
@@ -1,3 +1,12 @@
+SESSION_PREFIX="tmp-"
+
 if [ -z "$TMUX" ]; then
-    tmux new
+    for (( i=0; ; i++ ))
+    do
+        if ! tmux has-session -t=$SESSION_PREFIX$i 2> /dev/null
+        then
+            tmux new-session -s $SESSION_PREFIX$i;
+            break
+        fi
+    done
 fi
