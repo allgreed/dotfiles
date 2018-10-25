b46e6513b4331fcfe7de4e6cf6d4380ec943cca4
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2018-10-25 19:43:47 +0200

    Add desired session-killing logic

--- scripts/run-tmux.sh
+++ scripts/run-tmux.sh
@@ -1,11 +1,17 @@
 SESSION_PREFIX="tmp-"
 
 if [ -z "$TMUX" ]; then
+    tmux list-sessions -F "#{session_name}: #{?session_attached,attached,dettached}" \
+        | grep ": dettached" \
+        | cut -d: -f 1 \
+        | grep -P '^tmp-\d+$' \
+        | xargs -n 1 tmux kill-session -t
+
     for (( i=0; ; i++ ))
     do
         if ! tmux has-session -t=$SESSION_PREFIX$i 2> /dev/null
         then
-            tmux new-session -s $SESSION_PREFIX$i;
+            tmux new-session -s $SESSION_PREFIX$i
             break
         fi
     done
