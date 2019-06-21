11961787f88b56c3d56c68f20ae49efd2d799ea5
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-06-21 22:20:04 +0200

    Order manifest alphabetically

--- nix/manifest.nix
+++ nix/manifest.nix
@@ -10,27 +10,18 @@
   }
   {
     meta = {};
-    name = "go-1.12.1";
-    out = { outPath = "/nix/store/749qksf79hvn0aprcznd9bwfv550qwh3-go-1.12.1"; };
-    outPath = "/nix/store/749qksf79hvn0aprcznd9bwfv550qwh3-go-1.12.1";
-    outputs = [ "out" ];
-    system = "x86_64-linux";
-    type = "derivation";
-  }
-  {
-    meta = {};
-    name = "nodejs-12.2.0";
-    out = { outPath = "/nix/store/vf9wmbffxisl1v26c90z9aj9hrrg5mig-nodejs-12.2.0"; };
-    outPath = "/nix/store/vf9wmbffxisl1v26c90z9aj9hrrg5mig-nodejs-12.2.0";
-    outputs = [ "out" ];
-    system = "x86_64-linux";
-    type = "derivation";
-  }
-  {
-    meta = {};
     name = "git-2.21.0";
     out = { outPath = "/nix/store/czm6spl5g0ig57alx06apjjg9lrc657k-git-2.21.0"; };
     outPath = "/nix/store/czm6spl5g0ig57alx06apjjg9lrc657k-git-2.21.0";
+    outputs = [ "out" ];
+    system = "x86_64-linux";
+    type = "derivation";
+  }
+  {
+    meta = {};
+    name = "go-1.12.1";
+    out = { outPath = "/nix/store/749qksf79hvn0aprcznd9bwfv550qwh3-go-1.12.1"; };
+    outPath = "/nix/store/749qksf79hvn0aprcznd9bwfv550qwh3-go-1.12.1";
     outputs = [ "out" ];
     system = "x86_64-linux";
     type = "derivation";
@@ -42,6 +33,15 @@
     outPath = "/nix/store/hbhdjn5ik3byg642d1m11k3k3s0kn3py-nix-2.2.2";
     outputs = [ "out" ];
     system = "unknown";
+    type = "derivation";
+  }
+  {
+    meta = {};
+    name = "nodejs-12.2.0";
+    out = { outPath = "/nix/store/vf9wmbffxisl1v26c90z9aj9hrrg5mig-nodejs-12.2.0"; };
+    outPath = "/nix/store/vf9wmbffxisl1v26c90z9aj9hrrg5mig-nodejs-12.2.0";
+    outputs = [ "out" ];
+    system = "x86_64-linux";
     type = "derivation";
   }
   {
