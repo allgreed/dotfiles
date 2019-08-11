da9851a99ba68aca1c646b1bdb67e0e9b0a91fe8
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-08-11 20:27:53 +0200

    Change monitors

--- screenlayout/onlyexternal.sh
+++ screenlayout/onlyexternal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output LVDS-1 --off --output VGA-1 --primary --mode 1680x1050 --pos 0x0 --rotate normal
+xrandr --output LVDS-1 --off --output VGA-1 --mode 1920x1080 --pos 0x0 --rotate normal
 ~/.fehbg
