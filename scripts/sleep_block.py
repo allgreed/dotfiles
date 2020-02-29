9a8d83928654e313182c76c92e8bc4dbbc8797a7
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2020-02-29 14:56:33 +0100

    Use .Xresources for colors

--- scripts/sleep_block.py
+++ scripts/sleep_block.py
@@ -1,23 +1,41 @@
 #!/usr/bin/env python3
 from datetime import datetime, timedelta
+import subprocess
 
-now = datetime.now()
+def py_xgetres(res: str):
+    """A really shitty ad hoc implementation of xgetres, totally non-portable and probably will fail for your .Xresources
+    
+    Feel free to replce it with a legit exec once someone ports xgetres from aur(tm) to nix
+    """
+    p = subprocess.run(["xrdb", "-query"], capture_output=True)
+    entry_tokens = p.stdout.decode("utf-8").split("\n")
+    fuj_entries = map(lambda s: s.split(":"), entry_tokens)
+    entries = filter(lambda l: len(l) == 2, fuj_entries)
+    resource_map = { k.lstrip("*"): v.lstrip() for k, v in entries }
+    return resource_map[res.lstrip(".")]
 
-next_day_hours = 24 if now.hour >= 9 else 0
-now_date = now.date()
 
-refernce = datetime(now_date.year, now_date.month, now_date.day, 9) + timedelta(hours=next_day_hours)
+def main():
+    now = datetime.now()
 
-diff = refernce - now
+    next_day_hours = 24 if now.hour >= 9 else 0
+    now_date = now.date()
 
-# TODO: use xresources
-if diff > timedelta(hours=8):
-    color = "#93a1a1" # 14
-elif diff > timedelta(hours=6):
-    color = "#b58900" # 3
-else:
-    color = "#dc322f" # 1
+    refernce = datetime(now_date.year, now_date.month, now_date.day, 9) + timedelta(hours=next_day_hours)
 
-formatted_diff = str(timedelta(seconds=diff.seconds))
+    diff = refernce - now
 
-print("{0}\n{0}\n{1}".format(formatted_diff, color))
+    if diff > timedelta(hours=8):
+        color = py_xgetres(".color14")
+    elif diff > timedelta(hours=6):
+        color = py_xgetres(".color13")
+    else:
+        color = py_xgetres(".color1")
+
+    formatted_diff = str(timedelta(seconds=diff.seconds))
+
+    print("{0}\n{0}\n{1}".format(formatted_diff, color))
+
+if __name__ == "__main__":
+    main()
+
