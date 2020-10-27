00b0f10121195a9d9d87678bccc1b4f7421cb9bd
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2020-10-28 00:33:57 +0100

    Fix the locking script and enhance security
    esentially escrotum doesn't have an option to not beep as it doesn't
    beep by default xD. Also: added suport for controlling volume even when
    the screen is locked and I'm purging the ssh-agent on every lock

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,26 +1,17 @@
 #!/usr/bin/env bash
+set -o xtrace
 
 # prepare
-###############################
-# saves the state of mute
-#~/aliases/volume mstat > /tmp/muted 
-screen_path="/media/ramdisk/screen-dump.png" # in mem path is a good idea
-scrot -z $screen_path
+screen_path="/tmp/screen-dump.png"
+escrotum $screen_path
 convert $screen_path -filter point -resize 10% -resize 1000% -paint 3 $screen_path
 
-# lock
-###############################
-#~/aliases/volume mute
+# pre-lock
 physlock -lds
-i3lock -n -i $screen_path
+ssh-add -D
+
+# actual lock
+i3lock-color -n -i $screen_path --ignore-empty-password --pass-volume-keys
 
 # unlock
-###############################
 physlock -Lds
-
-# returns to previous mute state
-#if [ `cat /tmp/muted` = "false" ]
-#then
-#    ~/aliases/volume unmute
-#fi
-
