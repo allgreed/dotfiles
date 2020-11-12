eb7663299d926ea54bc8af4b6676785692f49391
Olgierd "Allgreed" Kasprowicz <olgierd.kasprowicz@dynatrace.com>
2020-11-12 14:38:48 +0100

    Add nix manifest for commonly used stuff

---
+++ nix/manifest.nix
@@ -0,0 +1,57 @@
+[
+  {
+    man = { outPath = "/nix/store/nv1nz46nglvdgnzvv0kyf2bfb82bjpq4-escrotum-2019-06-10-man"; };
+    meta = {};
+    name = "escrotum-2019-06-10";
+    out = { outPath = "/nix/store/mli2zgw8as9gv29nsp43szb67cvcgj3n-escrotum-2019-06-10"; };
+    outPath = "/nix/store/mli2zgw8as9gv29nsp43szb67cvcgj3n-escrotum-2019-06-10";
+    outputs = [ "man" "out" ];
+    system = "x86_64-linux";
+    type = "derivation";
+  }
+  {
+    meta = {};
+    name = "home-manager-path";
+    out = { outPath = "/nix/store/ja062dyyyf3661wpjny51sx0qbv9sm9k-home-manager-path"; };
+    outPath = "/nix/store/ja062dyyyf3661wpjny51sx0qbv9sm9k-home-manager-path";
+    outputs = [ "out" ];
+    type = "derivation";
+  }
+  {
+    meta = {};
+    name = "i3blocks-1.5";
+    out = { outPath = "/nix/store/q5chcd4jxj4ghhqcdaa84xfqrda977k0-i3blocks-1.5"; };
+    outPath = "/nix/store/q5chcd4jxj4ghhqcdaa84xfqrda977k0-i3blocks-1.5";
+    outputs = [ "out" ];
+    system = "x86_64-linux";
+    type = "derivation";
+  }
+  {
+    man = { outPath = "/nix/store/qg9xnyjvr1rq1nif15dng94p34ihaxzn-kubectl-1.19.3-man"; };
+    meta = {};
+    name = "kubectl-1.19.3";
+    out = { outPath = "/nix/store/cybqlza9mhk1qs18j4qpmpva6611vdz1-kubectl-1.19.3"; };
+    outPath = "/nix/store/cybqlza9mhk1qs18j4qpmpva6611vdz1-kubectl-1.19.3";
+    outputs = [ "man" "out" ];
+    system = "x86_64-linux";
+    type = "derivation";
+  }
+  {
+    meta = {};
+    name = "nix-2.3.8";
+    out = { outPath = "/nix/store/qxayqjmlpqnmwg5yfsjjayw220ls8i2r-nix-2.3.8"; };
+    outPath = "/nix/store/qxayqjmlpqnmwg5yfsjjayw220ls8i2r-nix-2.3.8";
+    outputs = [ "out" ];
+    system = "unknown";
+    type = "derivation";
+  }
+  {
+    meta = {};
+    name = "spotify-1.1.42.622.gbd112320-37";
+    out = { outPath = "/nix/store/iq1nqq8d159facmzbrlxx539bd5y81rp-spotify-1.1.42.622.gbd112320-37"; };
+    outPath = "/nix/store/iq1nqq8d159facmzbrlxx539bd5y81rp-spotify-1.1.42.622.gbd112320-37";
+    outputs = [ "out" ];
+    system = "x86_64-linux";
+    type = "derivation";
+  }
+]
