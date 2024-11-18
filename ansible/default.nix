f9a9b5f3a2bbc31b0fb51c6e146710031a2e52f6
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2024-11-18 22:42:17 +0100

    Fix reproducibility issues

--- ansible/default.nix
+++ ansible/default.nix
@@ -1,13 +1,19 @@
-with import <nixpkgs> {};
 let
-  pythonCore = python3;
+  pkgs = import (builtins.fetchGit {
+    url = "https://github.com/nixos/nixpkgs/";
+    ref = "refs/heads/nixos-unstable";
+    rev = "5e4fbfb6b3de1aa2872b76d49fafc942626e2add"; # 18-11-2024 
+    # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
+  }) { config = {}; };
+
+  pythonCore = pkgs.python3;
   pythonPkgs = python-packages: with python-packages; [
       setuptools
     ]; 
   myPython = pythonCore.withPackages pythonPkgs;
 in
-mkShell {
-  buildInputs = 
+pkgs.mkShell {
+  buildInputs = with pkgs;
     [
       git
       ansible
